ef7636667763a4c96381895006a72e2976eb5383
SamuelSJ17 <samuel.jaimez17@gmail.com>
2018-06-29 07:57:33 -0600

    Revert "Merge remote-tracking branch 'origin/virginia'"
    This reverts commit f4e70f8c342b708b04a57be1a9cbde7ece250eef, reversing
    changes made to cb2185a8b88d9889cece15a17950b598f8677a85.

--- apps/proveedor/forms.py
+++ apps/proveedor/forms.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django import forms
 from .models import *
 
@@ -31,53 +30,3 @@ class contactoForm(forms.ModelForm):
 		'nombre':'Nombre del contacto',
 		'telefono':'Telefono del contacto',
 		}
-=======
-from django import forms
-from .models import *
-
-class proveedorForm(forms.ModelForm):
-	class Meta:
-		model=Proveedor
-		fields={
-		'nombre',
-		'telefono',
-		'direccion',
-		'email',
-		}
-		labels={
-		'nombre':'Nombre del proveedor',
-		'telefono':'Numero telefonico',
-		'direccion':'Direccion',
-		'email':'Correo electronico',
-		}
-# agrega el estilo a los inputs
-	def __init__(self, *args, **kwargs):
-		super(proveedorForm, self).__init__(*args, **kwargs)
-		self.fields['nombre'].widget.attrs.update({'class': 'mdl-textfield__input'})
-		self.fields['telefono'].widget.attrs.update({'class': 'mdl-textfield__input', 'size': 10})
-		self.fields['direccion'].widget.attrs.update({'class': 'mdl-textfield__input'})
-		self.fields['email'].widget.attrs.update({'class': 'mdl-textfield__input'})
-
-class contactoForm(forms.ModelForm):
-	class Meta:
-		model=ContactoProveedor
-		fields={
-		'nombre',
-		'telefono',
-		}
-		labels={
-		'nombre':'Nombre del contacto',
-		'telefono':'Telefono del contacto',
-		}
-		errors={
-		'nombre':'',
-		'telefono':'Numero de telefono invalido'
-		}
-# agrega el estilo a los inputs		
-	def __init__(self, *args, **kwargs):
-		super(contactoForm, self).__init__(*args, **kwargs)
-		self.fields['nombre'].widget.attrs.update({'class': 'mdl-textfield__input'})
-		self.fields['telefono'].widget.attrs.update({'class': 'mdl-textfield__input'})
-
-		
->>>>>>> origin/virginia
--- apps/proveedor/urls.py
+++ apps/proveedor/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from __future__ import unicode_literals
 from __future__ import absolute_import
 
@@ -18,25 +17,4 @@ urlpatterns =[
 	url(r'^eliminarContacto/(?P<id_contacto>\d+)/$', eliminarContacto, name="elim_contacto"),
 	url(r'^editarProveedor/(?P<id_proveedor>\d+)/$', editarProveedor, name="edit_proveedor"),
 	url(r'^editarContacto/(?P<id_contacto>\d+)/$', editarContacto, name="edit_contacto"),
-=======
-from __future__ import unicode_literals
-from __future__ import absolute_import
-
-from django.conf.urls import url, include
-from .views import *
-from . import views
-
-app_name = 'proveedor'
-
-urlpatterns =[
-
-	#USUARIO
-	url(r'^crearProveedor$', crearProveedor, name="crear_proveedor"),
-	url(r'^gestion$', gestionProveedor, name="gestion_proveedor"),
-	url(r'^contacto/(?P<id_proveedor>\d+)/$', crearContacto,name="crear_contacto"),
-	url(r'^eliminarProveedor/(?P<id_proveedor>\d+)/$', eliminarProveedor, name="elim_proveedor"),
-	url(r'^eliminarContacto/(?P<id_contacto>\d+)/$', eliminarContacto, name="elim_contacto"),
-	url(r'^editarProveedor/(?P<id_proveedor>\d+)/$', editarProveedor, name="edit_proveedor"),
-	url(r'^editarContacto/(?P<id_contacto>\d+)/$', editarContacto, name="edit_contacto"),
->>>>>>> origin/virginia
 ]
