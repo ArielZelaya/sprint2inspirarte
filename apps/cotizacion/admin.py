ae849143993ea04e5f1fadd36dd2c46955a63acc
unknown <samuel.jaimez17@gmail.com>
2018-09-12 16:05:31 -0600

    Modelo cotizacion ultracorregido

--- apps/cotizacion/admin.py
+++ apps/cotizacion/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(TipoProducto)
+admin.site.register(Cotizacion)
+admin.site.register(DetalleCotizacion)
+admin.site.register(PrecioProducto)
+admin.site.register(TamanioProducto)
