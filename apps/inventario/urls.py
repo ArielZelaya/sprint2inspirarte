0f5b318726aa4a689783777c72c2f3a25c619748
Edwin Molina <edwin.ues.8@gmail.com>
2018-09-17 00:59:34 -0600

    Inventario - completo

--- apps/inventario/urls.py
+++ apps/inventario/urls.py
@@ -1,3 +1,9 @@
+# dsi2018\apps\inventario\urls.py
+# MODULO: inventario
+# OJETIVO: almacenar las urls correpondientes a las views (controladores) del modulo inventario
+# ANALISTA PROGRAMADOR: Edwin Molina,	FECHA CREACION: 18/08/2018
+# MODIFICADO POR: Edwin Molina,			FECHA MODIFICACION: 16/09/2018
+
 from __future__ import unicode_literals
 from __future__ import absolute_import
 
@@ -10,5 +16,9 @@ urlpatterns =[
 
 	#ADMINISTRAR PRODUCTOS
 	url(r'^administrarProductos', adminProductos ,name="adm_pro"),
+	url(r'^editarProducto/(?P<id_producto>\d+)$', editarProducto, name='edt_pro'),
+	url(r'^proveedoresProducto/(?P<id_producto>\d+)$', consultarProveedores, name='cop_prv'),
+	url(r'^desvincularProveedor/(?P<id_producto>\d+)//(?P<id_proveedor>\d+)/$', desvincularProveedor, name="des_prv"),
+	#REGISTRO DE INVENTARIO
 	url(r'^registroInventario$', regInventario ,name="reg_inv"),
 ]
